035be491878d9e8ced3c1ac49b54d7655a06546a
Joshua Lansford <Joshua+git@laserlinc.com>
2017-01-27 08:47:19 -0500

    Added beeping which which starts if the message isn't dismissed soon enough

--- stay_awake.py
+++ stay_awake.py
@@ -1,3 +1,4 @@
+from threading import Thread,Event
 import easygui
 import time
 
@@ -5,14 +6,38 @@ delay = 10
 
 done = False
 
+
+class Beeper(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self._stop = Event()
+
+    def run(self):
+        self._stop.clear()
+        time.sleep(3)
+        i = 0
+        while not self._stop.is_set() and i < 20:
+            print "\a"
+            time.sleep(0.2)
+            i += 1
+            
+    def stop(self):
+        self._stop.set()
+
+
+
 while not done:
     time.sleep( delay )
+
+    beeper = Beeper()
 
     #make a default one which can get accidentally pressed without making the dialog go away.
     choices = [ ":-)", "^", "v", "exit" ]
     choice = ":-)"
+    beeper.start()
     while choice == ":-)":
         choice = easygui.buttonbox( str(delay) + " status?", choices=choices )
+    beeper.stop()
 
     if choice == "^":
         delay /= .8
