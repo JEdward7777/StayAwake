6be75f82a90574ee01f6f202f37f1f90cbc1278b
Joshua Lansford <HootOwl7777@gmail.com>
2025-05-13 10:50:36 -0400

    Made the sh script work from other directories.

--- stay_awake.sh
+++ stay_awake.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-. venv/bin/activate
-python stay_awake.py "$@"
+
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Activate the virtual environment
+source "$SCRIPT_DIR/venv/bin/activate"
+
+# Run the Python script with any passed arguments
+python "$SCRIPT_DIR/stay_awake.py" "$@"
+
 #read -n1 -r -p "Press any key to continue..." key
